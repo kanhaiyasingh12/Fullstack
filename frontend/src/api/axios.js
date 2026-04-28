fc1f59a9e02f98a2e656d8c596dc986680dd237c
kanhaiyasingh12 <kanhaiyajee1441@gmail.com>
2026-04-28 15:52:15 +0530

    updated axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const defaultBaseUrl = `${window.location.protocol}//${window.location.hostname}:5000/api`;
+
 const api = axios.create({
   // This pulls the URL from your .env file
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || defaultBaseUrl
 });
 
 export default api;
